d632c4327b2c423138002656b1302e7bc1326e42
piotrbod <peter.bod@gmail.com>
2020-03-20 22:32:44 +0000

    Update fix_duplicate_columns.sh

--- fix_duplicate_columns.sh
+++ fix_duplicate_columns.sh
@@ -1,32 +1,49 @@
 #!/bin/bash
 
 allservicesdir="allservices"
+loadfiles="../allfiles.txt"
 
 cd $allservicesdir
+ls -Al | tail -n +2 | tr -s ' ' | cut -f9- -d' ' > $loadfiles
 
-ls -Al | tail -n +2 | tr -s ' ' | cut -f9- -d' ' > "../allfiles.txt"
+### Remove redundant comments from each file ###
+allfiles="../allfiles.txt"
 
-input="../allfiles.txt"
-
+input="$allfiles"
 while IFS= read -r line
 do
 
-        columnno=`cat $line | sed -n '1p' | awk -F',' 'NR==1{print NF}'`
+        echo "$line"
+
         servicename=`echo $line | cut -d "-" -f2 | cut -d "." -f1`
+        colvalue=`head -n 1 $line | cut -d "," -f1 | sed s/\"//g`
+
+        if [ "$colvalue" == "SKU" ]; then
+
+                echo ""
+                echo "You did run this script before. Run download_prices.sh first and let it clean working directories."
+                echo ""
+                exit 0
+
+        fi
+
+        sed -i -e 1,5d $line
+
+        columnno=`cat $line | sed -n '1p' | awk -F',' 'NR==1{print NF}'`
 
         x=1
-
         while [ $x -le $columnno ]
         do
 
                 colvalue=`head -n 1 $line | cut -d "," -f$x | sed s/\"//g`
 
+                ### if there are more duplicates while crawling in Glue add more exceptions after storage\|deployment below ###
 
-### if there are more duplicates while crawling in Glue add more exceptions after storage|\deployment below ###
-
-                isitstorage=`echo $colvalue | grep -i 'storage|\deployment'`
+                isitstorage=`echo $colvalue | grep -i 'storage\|deployment'`
 
                 if [ ! -z "$isitstorage" ]; then
+
+                        echo "isitstorage value is: $isitstorage"
 
                         sed -i -e "1s|$colvalue|$servicename-$colvalue|1" $line
 
